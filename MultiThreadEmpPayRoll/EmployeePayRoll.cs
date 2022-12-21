83e7f31576e8b67aa58331868d4016b969795d64
Dishant Dilip Yashwante <113447476+dishant1996@users.noreply.github.com>
2022-12-21 19:58:26 +0530

    TimeTakenByThread

---
+++ MultiThreadEmpPayRoll/EmployeePayRoll.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreadEmpPayRoll
+{
+    public class EmployeePayRoll
+    {
+        public List<EmpData> employeePolyeeDetailList = new List<EmpData>();
+
+        //UC 1
+        public void addEmployeeToPayroll(List<EmpData> employeePayrollDataList)
+        {
+            employeePayrollDataList.ForEach(employeeData =>
+            {
+                Console.WriteLine("Employee Being Added :" + employeeData.EmployeeName);
+                this.addEmployeeToPayroll(employeeData);
+                Console.WriteLine("Employee added : " + employeeData.EmployeeName);
+            });
+            Console.WriteLine(this.employeePolyeeDetailList.ToString());
+        }
+        //UC 2
+        public void addEmployeeToPayrollWithThread(List<EmpData> employeePayrollDataList)
+        {
+            employeePayrollDataList.ForEach(employeeData =>
+            {
+                Task thread = new Task(() =>
+                {
+                    //For Every iterarion of foreach loop, every record will get in to thread, every thread will be
+                    //running in a single process to make execution quick and fast.
+
+                    Console.WriteLine("Employee Being Added :" + employeeData.EmployeeName);
+                    this.addEmployeeToPayroll(employeeData);
+                    Console.WriteLine("Employee added : " + employeeData.EmployeeName);
+                });
+                thread.Start();
+            });
+            Console.WriteLine(this.employeePolyeeDetailList.Count);
+        }
+        public void addEmployeeToPayroll(EmpData emp)
+        {
+            employeePolyeeDetailList.Add(emp);
+        }
+    }
+}
